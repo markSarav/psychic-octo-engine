dcd5b8e37075eeede8100205d3fd09c02554c6e1
markSarav <mark.saravia@gmail.com>
2024-05-14 17:57:24 -0400

    added left and right movement to player's ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,13 +24,9 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
             self._check_events()
-            
-            # Redraw the screen during each pass through the loop.
+            self.ship.update()
             self._update_screen()
-
-            # Make the most recently drawn screen visible.
             self.clock.tick(60)
     
     def _check_events(self):
@@ -38,6 +34,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
