cf167db352f18fa7adc93e7410138d17340b794f
markSarav <mark.saravia@gmail.com>
2024-05-17 18:28:33 -0400

    added Bullet.py and ability to fire bullets with spacebar from Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from alien_ship import AlienShip
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,15 +12,16 @@ class AlienInvasion:
     def __init__(self) -> None:
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Alien Invasion")
         self.settings = Settings()
+        self.clock = pygame.time.Clock()
         # self.screen = pygame.display.set_mode(
             # (self.settings.screen_width, self.settings.screen_height)
         # )
+        self.bullets = pygame.sprite.Group()
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height  = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
         self.alien_ship = AlienShip(self)
@@ -29,6 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
     
@@ -44,23 +47,32 @@ class AlienInvasion:
     
     def _check_keydown_events(self, event):
         """Respond to key presses."""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_a:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _check_keyup_events(self, event):
         """Resond to key releases."""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_a:
             self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         self.alien_ship.blitme()
         pygame.display.flip()
